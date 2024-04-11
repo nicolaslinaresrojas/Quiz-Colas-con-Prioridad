f2febb1d5531a00380525a043914a90055e6a3dd
nicolaslinaresrojas <159267707+nicolaslinaresrojas@users.noreply.github.com>
2024-04-10 23:58:51 -0500

    Quiz Cola de Prioridad - Nicolas Linares.py

--- Quiz Cola de Prioridad - Nicolas Linares.py
+++ Quiz Cola de Prioridad - Nicolas Linares.py
@@ -1,128 +1,140 @@
+# Nicolas Linares 2230027 - Quiz Monticulos
+
 class MonticuloBinario:
     def __init__(self):
-        self.listaMonticulo = []
+        self.listaMonticulo = [0]
         self.tamanoActual = 0
-        
-    def infiltArriba(self, i):
-        while i // 2 > 0:
-            if self.listaMonticulo[i][0] < self.listaMonticulo[i // 2][0]:
-                self.listaMonticulo[i], self.listaMonticulo[i // 2] = self.listaMonticulo[i // 2], self.listaMonticulo[i]
-                i = i // 2
 
-    def insertar(self, llamada):
-        self.listaMonticulo.append(llamada)
-        self.tamanoActual += 1
-        self.infiltArriba(self.tamanoActual)
-        
-    def infiltAbajo(self, i):
-        while (i * 2) <= self.tamanoActual:
-            hijoMin = self.hijoMin(i)
-            if self.listaMonticulo[i][0] > self.listaMonticulo[hijoMin][0]:
-                self.listaMonticulo[i], self.listaMonticulo[hijoMin] = self.listaMonticulo[hijoMin], self.listaMonticulo[i]
-                i = hijoMin
-                
-    def hijoMin(self, i):
-        if i * 2 + 1 > self.tamanoActual:
-            return i * 2
-        else:
-            if self.listaMonticulo[i * 2][0] < self.listaMonticulo[i * 2 + 1][0]:
-                return i * 2
-            else:
-                return i * 2 + 1
+
+    def infiltArriba(self,i):
+        while i // 2 > 0:
+          if self.listaMonticulo[i] < self.listaMonticulo[i // 2]:
+             tmp = self.listaMonticulo[i // 2]
+             self.listaMonticulo[i // 2] = self.listaMonticulo[i]
+             self.listaMonticulo[i] = tmp
+          i = i // 2
+
+    def insertar(self,k):
+      self.listaMonticulo.append(k)
+      self.tamanoActual = self.tamanoActual + 1
+      self.infiltArriba(self.tamanoActual)
+
+    def infiltAbajo(self,i):
+      while (i * 2) <= self.tamanoActual:
+          hm = self.hijoMin(i)
+          if self.listaMonticulo[i] > self.listaMonticulo[hm]:
+              tmp = self.listaMonticulo[i]
+              self.listaMonticulo[i] = self.listaMonticulo[hm]
+              self.listaMonticulo[hm] = tmp
+          i = hm
+
+    def hijoMin(self,i):
+      if i * 2 + 1 > self.tamanoActual:
+          return i * 2
+      else:
+          if self.listaMonticulo[i*2] < self.listaMonticulo[i*2+1]:
+              return i * 2
+          else:
+              return i * 2 + 1
 
     def eliminarMin(self):
-        valorSacado = self.listaMonticulo[1]
-        self.listaMonticulo[1] = self.listaMonticulo[self.tamanoActual]
-        self.tamanoActual -= 1
-        self.listaMonticulo.pop()
-        self.infiltAbajo(1)
-        return valorSacado
+      valorSacado = self.listaMonticulo[1]
+      self.listaMonticulo[1] = self.listaMonticulo[self.tamanoActual]
+      self.tamanoActual = self.tamanoActual - 1
+      self.listaMonticulo.pop()
+      self.infiltAbajo(1)
+      return valorSacado
 
-class Llamada:
-    
+    def construirMonticulo(self,unaLista):
+      i = len(unaLista) // 2
+      self.tamanoActual = len(unaLista)
+      self.listaMonticulo = [0] + unaLista[:]
+      while (i > 0):
+          self.infiltAbajo(i)
+          i = i - 1
+
+class Persona:
     def __init__(self, nombre, edad, direccion, motivo, gravedad):
         self.nombre = nombre
         self.edad = edad
         self.direccion = direccion
         self.motivo = motivo
         self.gravedad = gravedad
-    
-    def __lt__(self, other):
-        if self.gravedad == other.gravedad:
-            if self.edad < 12:
-                return True
-            elif self.edad >= 65:
-                return False
-            elif other.edad < 12:
-                return False
-            elif other.edad >= 65:
-                return True
-            else:
-                return False
-        else:
-            return self.gravedad < other.gravedad
-        
+
+    def __str__(self):
+        return f"Nombre: {self.nombre}, Edad: {self.edad}, Direccion: {self.direccion}, Motivo: {self.motivo}, Gravedad: {self.gravedad}"
+
 class ColaPrioridad:
     def __init__(self):
-        self.monticulo = MonticuloBinario()
-                
-    def ingresar_llamada(self, llamada):
-        self.monticulo.insertar(llamada)
-        print("Llamada ingresada. Posición en la cola:", self.monticulo.tamanoActual)
-        
-    def pasar_siguiente_solicitud(self):
-        if self.monticulo.tamanoActual > 0:
-            llamada = self.monticulo.eliminarMin()
-            print("Siguiente solicitud a ser atendida:")
-            print("Nombre:", llamada.nombre)
-            print("Edad:", llamada.edad)
-            print("Direccion:", llamada.direccion)
-            print("Motivo:", llamada.motivo)
-            print("Gravedad:", llamada.gravedad)
+        self.cola = MonticuloBinario()
+
+    def ingresar_llamada(self):
+        nombre = input("Ingresa el nombre: ")
+        edad = int(input("Ingresa la edad: "))
+        direccion = input("Ingresa la direccion: ")
+        motivo = input("Ingresa el motivo de la llamada: ")
+        gravedad = int(input("Ingresa la gravedad del 1 al 5: "))
+
+        gravedades = [llamada[1] for llamada in self.cola.listaMonticulo[1:]]
+        if gravedad in gravedades:
+            if edad < 12:
+                prioridad = 1
+            elif edad >= 65:
+                prioridad = 2
+            else:
+                prioridad = 4
         else:
-            print("No hay solicitudes pendientes.")
-            
+            if edad < 12:
+                prioridad = 1
+            elif edad >= 65:
+                prioridad = 2
+            else:
+                prioridad = 4
+
+        persona = Persona(nombre, edad, direccion, motivo, gravedad)
+        self.cola.insertar((prioridad, gravedad, persona))
+
+        print(f"{persona.nombre} fue añadida a la cola con prioridad {prioridad}.")
+
+    def siguiente_solicitud(self):
+        if self.cola.tamanoActual == 0:
+            print("No hay solicitudes en la cola")
+            return
+
+        siguiente = self.cola.eliminarMin()
+        print("Siguiente solicitud a atender:")
+        print(siguiente[2]) 
+        print(f"Prioridad: {siguiente[0]}, Gravedad: {siguiente[1]}")
+
     def mostrar_cola(self):
-        print("Cola de atención en este momento:")
-        for i in range(1, self.monticulo.tamanoActual + 1):
-            llamada = self.monticulo.listaMonticulo[i]
-            print("Posicion:", i)
-            print("Nombre:", llamada.nombre)
-            print("Edad:", llamada.edad)
-            print("Direccion:", llamada.direccion)
-            print("Motivo:", llamada.motivo)
-            print("Gravedad:", llamada.gravedad)
-            
-def menu():
-        print("\nMenú:")
-        print("1. Ingresar Llamada")
-        print("2. Pasar siguiente solicitud")
-        print("3. Mostrar la cola")
-        print("4. Salir")
-        
+        if self.cola.tamanoActual == 0:
+            print("La cola no existe")
+            return
+
+        print("Cola de atencion:")
+        for i in range(1, self.cola.tamanoActual + 1):
+            print(f"{i}. {self.cola.listaMonticulo[i][2]} - Prioridad: {self.cola.listaMonticulo[i][0]}, Gravedad: {self.cola.listaMonticulo[i][1]}")
+
+
 cola_prioridad = ColaPrioridad()
 
 while True:
-    
-    menu()
+    print("\nMenú:")
+    print("1) Ingresar Nueva Llamada")
+    print("2) Pasar siguiente solicitud")
+    print("3) Mostrar cola")
+    print("4) Salir")
+
     opcion = input("Seleccione una opción: ")
-        
+
     if opcion == "1":
-            
-            nombre = input("Ingrese nombre completo: ")
-            edad = int(input("Ingrese edad: "))
-            direccion = input("Ingrese direccion: ")
-            motivo = input("Ingrese motivo de la llamada: ")
-            gravedad = int(input("Ingrese gravedad (1-5): "))
-            llamada = Llamada(nombre, edad, direccion, motivo, gravedad)
-            cola_prioridad.ingresar_llamada(llamada)
-            
+        cola_prioridad.ingresar_llamada()
     elif opcion == "2":
-        cola_prioridad.pasar_siguiente_solicitud()
+        cola_prioridad.siguiente_solicitud()
     elif opcion == "3":
         cola_prioridad.mostrar_cola()
     elif opcion == "4":
         print("Saliendo")
         break
     else:
-        print("Opcion Invalida")
+        print("Opcion no valida")
